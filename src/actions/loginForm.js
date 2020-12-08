54ed3c7d67d019e2313ddd4ed65fd18f673c0a4d
devludaley <daleyluis@gmail.com>
2020-12-08 16:29:01 -0500

    Update loginForm.js
    add console.log to check data values

--- src/actions/loginForm.js
+++ src/actions/loginForm.js
@@ -1,4 +1,5 @@
 export const updateLoginForm = (formaData) => {
+console.log('🚀 ~ file: loginForm.js ~ line 2 ~ updateLoginForm ~ formaData', formaData);
     return {
     type: "UPDATE_LOGIN_FORM",
     payload: formaData
