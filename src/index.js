3c22f7823d7d5d6feb43c2aa54b4b72c4863901e
devludaley <daleyluis@gmail.com>
2020-12-08 12:29:03 -0500

    add redux to index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose , combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+// import store from './store.js'
+// import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const store = createStore(
+  // reducer, )
+// const users = () => []
+const reducer = combineReducers({users
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//! redux store
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+
+
+// ! ReactDOM.render(<Provider store={ store }><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
