54c0ccc09350eeaebcb7702a7c5214a833ef997a
devludaley <daleyluis@gmail.com>
2020-12-09 18:07:13 -0500

    add componentDidMount

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Login from "./components/Login"
 import './App.css';
 
 class App extends Component {
-  // componentDidMount(){
+  componentDidMount(){
   //   fetch('http://localhost:3000/api/v1/users/2')
   //   .then(r=> r.json())
   //   .then(console.log)
-  // }
+  }
   render(){
     return (
     <Fragment className="App">
