6ddff42d70ab7213ae23ff1af06900c54ed150b1
devludaley <daleyluis@gmail.com>
2020-12-09 17:28:26 -0500

    Update actions/currentUser logic
    fix:
    - user call
    -add .then
    - .catch trigger
    -

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,23 +1,36 @@
 //synchronous action creators 
-export const currentUser = () => {
+export const setCurrentUser = (user) => {
     return {
         type: "SET_CURRENT_USER",
-        payload: "user"
+        // payload: user
+        user
     }
 }
  
 //asynchronous action creators 
 export const login = credentials => {
-    const url = 'http://localhost:3001/'
+    console.log('The value of credentials equals => ', credentials)
     return dispatch => {
-        return fetch(url, {
+        return fetch('http://localhost:3000/api/v1/login', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({username:"cpdundon", password:"password"})
+            body: JSON.stringify(credentials) //({username:"cpdundon", password:"password"})
 
         })
+        .then(r => r.json())
+        .then(user => {
+            if (user.error) {
+                alert("Error #1 - fly in the ointment", user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        // ! remember to investigate why the catch is being triggered
+        .catch(console.log("actions/currentUser failing on or around line 30"),
+            // alert("Error #2 - something isn't connected properly")
+            )
     }
 }
 //  currentUser; 
