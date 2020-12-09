f7e7dc1f5ef0416dee407cc2243aa338592aa508
devludaley <daleyluis@gmail.com>
2020-12-09 17:29:31 -0500

    add handleSubmit & updateLoginForm

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,46 +2,42 @@ import React from 'react'
 import { connect } from 'react-redux'
 // import { updateLoginForm } from "../actions/loginForm.js"
 import { updateLoginForm } from '../actions/loginForm'
-import { login } from '../actions/currentUser.js'
+import { login } from '../actions/currentUser'
 // const Login = (props) => {
 
-const Login = ({loginFormData, updateLoginForm, login}) => {
+const Login = ({ loginFormData, updateLoginForm, login }) => {
     const handleInputChange = event => {
         const { name, value } = event.target
-        const updateFormInfo = {
+        const updatedFormInfo = {
             ...loginFormData,
             [name]: value
         }
-        updateLoginForm(updateFormInfo)
+        updateLoginForm(updatedFormInfo)
+        console.log('🚀 ~ file: Login.js ~ line 13 ~ Login ~ loginFormData', loginFormData);
+        console.log('🚀 ~ file: Login.js ~ line 21 ~ Login ~ updatedFormInfo', updatedFormInfo);
     }
-// const Login = ({props}) => {
 
-    // var handleChange = (e) => {
-    
-// }
+  const handleSubmit = event => {
+    event.preventDefault()
+    login(loginFormData)
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    } 
-    
-    return (
+  return (
     <form onSubmit={handleSubmit}>
-        <input placeholder="username" value={loginFormData.username} onChange={handleInputChange} type="text" name="username"/>
-        <input placeholder="password" value={loginFormData.password} onChange={handleInputChange} type="text" name="password"/>
-        <input type="submit" value="Log In"/>
-    </form>  );
+      <input placeholder="username" value={loginFormData.username} name="username" type="text" onChange={handleInputChange} />
+      <input placeholder="password" value={loginFormData.password} name="password" type="text" onChange={handleInputChange} />
+      <input type="submit" value="Log In"/>
+    </form>
+  )
 }
 //! connect this component to the store. Ask for parts of the state from the store and add them to props
-const mapStateToProps = (state) => {
-    return{
-        username: state.loginFormData.username,
-        password: state.loginFormData.password
-    }
+const mapStateToProps = state => {
+  return {
+      // username: state.loginFormData.username,
+      // password: state.loginFormData.password
+    loginFormData: state.loginForm
+  }
 }
-// const mapDispatchToProps = (dispatch) => {
-    
-// }
- 
 
-// export default connect(null (Login))
-export default connect(mapStateToProps, { updateLoginForm } )(Login)
+
+export default connect(mapStateToProps, { updateLoginForm, login } )(Login)
