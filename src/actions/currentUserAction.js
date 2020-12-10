4d8cef337a785850e4bc9efde89fbfba64367204
devludaley <daleyluis@gmail.com>
2020-12-10 13:21:12 -0500

    add getCurrentUser method to currentUserAction

--- src/actions/currentUserAction.js
+++ src/actions/currentUserAction.js
@@ -33,4 +33,29 @@ export const login = credentials => {
             )
     }
 }
-//  currentUser; 
+
+export const getCurrentUser = credentials => {
+    console.log('The value of credentials equals => ', credentials)
+    return dispatch => {
+        return fetch('http://localhost:3000/api/v1/get_current_user', {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(r => r.json())
+        .then(response => {
+            if (response.error) {
+                alert("Error #1 - fly in the ointment", response.error)
+            } else {
+                dispatch(setCurrentUser(response.data))
+                // dispatch(getMyTrips(response.data))
+            }
+        })
+        // ! remember to investigate why the catch is being triggered
+        .catch(console.log("actions/currentUserAction => getCurrentUser failing on or around line 57"),
+             alert("getCurrentUser Error #2 - something isn't connected properly")
+            )
+    }
+}
